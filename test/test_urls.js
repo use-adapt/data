305d9ea89932efc7e9c6254ffb67a951d0eb8887
Zack Marvel <zpmarvel@gmail.com>
2017-02-07 10:33:03 -0500

    Tests for website URLs

--- test/test_urls.js
+++ test/test_urls.js
@@ -4,11 +4,12 @@ import * as http from 'http';
 import * as https from 'https';
 
 describe('project', function () {
+  const projects = Object.keys(data.projects).reduce(((acc, category) =>
+    acc.concat(data.projects[category])
+  ), []);
+
   describe('github URLs', function () {
     context('when not null', function () {
-      const projects = Object.keys(data.projects).reduce(((acc, category) =>
-        acc.concat(data.projects[category])
-      ), []);
       projects.forEach((project) => {
         it('should respond with 200 OK', function (done) {
           if (project.github === null) {
@@ -27,6 +28,32 @@ describe('project', function () {
           }
           else {
             http.get(project.github, done);
+          }
+        });
+      })
+    });
+  });
+  
+  describe('website URLs', function () {
+    context('when not null', function () {
+      projects.forEach((project) => {
+        it('should respond with 200 OK', function (done) {
+          if (project.website === null) {
+            done();
+          }
+          else if (project.website.startsWith('https')) {
+            https.get(project.website, (res) => {
+              res.resume();
+              if (res.statusCode === 200) {
+                done();
+              } else {
+                const err = new Error(`Request failed with status code ${res.statusCode}`);
+                done(err);
+              }
+            });
+          }
+          else {
+            http.get(project.website, done);
           }
         });
       })
