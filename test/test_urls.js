cfdd1baa2c6d1c69696450adc6bbb919f23b9bb3
Zack Marvel <zpmarvel@indiana.edu>
2017-02-08 10:45:11 -0500

    Improve detail of website and github tests; check for undefined websites

--- test/test_urls.js
+++ test/test_urls.js
@@ -1,7 +1,27 @@
 
-import * as data from '../data.json';
 import * as http from 'http';
 import * as https from 'https';
+
+import * as should from 'should';
+
+import * as data from '../data.json';
+
+function httpx_get(url, done) {
+  function callback(res) {
+    res.resume();
+    if (res.statusCode === 200) {
+      done();
+    } else {
+      const err = new Error(`Request for ${url} failed with status code ${res.statusCode}`);
+      done(err);
+    }
+  }
+
+  if (url.startsWith('https'))
+    https.get(url, callback);
+  else
+    http.get(url, callback);
+}
 
 describe('project', function () {
   const projects = Object.keys(data.projects).reduce(((acc, category) =>
@@ -10,55 +30,39 @@ describe('project', function () {
 
   describe('github URLs', function () {
     context('when not null', function () {
-      projects.forEach((project) => {
-        it('should respond with 200 OK', function (done) {
-          if (project.github === null) {
-            done();
-          }
-          else if (project.github.startsWith('https')) {
-            https.get(project.github, (res) => {
-              res.resume();
-              if (res.statusCode === 200) {
-                done();
-              } else {
-                const err = new Error(`Request failed with status code ${res.statusCode}`);
-                done(err);
-              }
+      projects.filter((project) => project.github !== null)
+        .forEach((project) => {
+          context(project.github, function () {
+            it('should respond with 200 OK', function (done) {
+              httpx_get(project.github, done);
             });
-          }
-          else {
-            http.get(project.github, done);
-          }
+          });
         });
-      })
     });
   });
-  
+
   describe('website URLs', function () {
-    context('when not null', function () {
-      projects.forEach((project) => {
-        it('should respond with 200 OK', function (done) {
-          if (project.website === null) {
-            done();
-          }
-          else if (project.website.startsWith('https')) {
-            https.get(project.website, (res) => {
-              res.resume();
-              if (res.statusCode === 200) {
-                done();
-              } else {
-                const err = new Error(`Request failed with status code ${res.statusCode}`);
-                done(err);
-              }
-            });
-          }
-          else {
-            http.get(project.website, done);
+    projects.filter((project) => project.website !== null)
+      .forEach((project) => {
+        context(`${project.name}`, function () {
+          it('should not be undefined'), function () {
+            project.name.should.not.be.undefined();
           }
         });
-      })
+      });
+
+    context('when not null', function () {
+      projects.filter((project) => project.website !== null)
+        .forEach((project) => {
+          context(`${project.name} (${project.website})`, function () {
+            it('should respond with 200 OK', function (done) {
+              httpx_get(project.website, done);
+            });
+          });
+        });
     });
   });
+
   describe('image URLs', function () {
     context('when not null', function () {
       projects.forEach((project) => {
