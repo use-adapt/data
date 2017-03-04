863f1406a1085d6eb3572453a855726c965a51f0
Jessie Pusateri <jessiepusateri@gmail.com>
2017-03-04 03:28:32 -0500

    writing tests/making changes to data/requirements to sync up configuration components and master list of requirements
    Remove 'immutable' dependency

--- test/test_urls.js
+++ test/test_urls.js
@@ -5,6 +5,8 @@ import * as https from 'https';
 import * as should from 'should';
 
 import * as data from '../data.json';
+import * as master_requirements from '../requirements.json';
+
 
 function httpx_get(url, done) {
   function callback(res) {
@@ -27,13 +29,14 @@ describe('project', function () {
   const projects = Object.keys(data.projects).reduce(((acc, category) =>
     acc.concat(data.projects[category])
   ), []);
-
+  const requirements = master_requirements.requirements;
+  
   describe('github URLs', function () {
     context('when not null', function () {
       projects.filter((project) => project.github !== null)
         .forEach((project) => {
           context(`${project.name} (${project.website})`, function () {
-            it('should not be undefined', function () {
+            it('should not be undefined (Accepts: \'github\': null or \'github\': \'VALID URL\')', function () {
               should.notStrictEqual(project.github, undefined);
             });
             it('should respond with 200 OK', function (done) {
@@ -49,7 +52,7 @@ describe('project', function () {
       projects.filter((project) => project.website !== null)
         .forEach((project) => {
           context(`${project.name} (${project.website})`, function () {
-            it('should not be undefined', function () {
+            it('should not be undefined (Accepts: \'website\': null or \'website\': \'VALID URL\')', function () {
               should.notStrictEqual(project.website, undefined);
             });
             it('should respond with 200 OK', function (done) {
@@ -65,7 +68,7 @@ describe('project', function () {
       projects.filter((project) => project.image !== null)
         .forEach((project) => {
           context(`${project.name} (${project.image})`, function () {
-            it('should not be undefined', function() {
+            it('should not be undefined (Accepts: \'image\': null or \'image\': \'VALID URL\')', function() {
               should.notStrictEqual(project.image, undefined);
             });
             it('should respond with 200 OK', function (done) {
@@ -73,6 +76,29 @@ describe('project', function () {
             });
           });
         });
+    });
+  });
+  describe('configurations', function () {
+    context('when not null', function () {
+      projects.filter((project) => project.configurations !== null)
+        .forEach((project) => {
+          context(`${project.name}`, function () {
+            context('should only use allowed requirements', function() {
+              project.configurations.forEach((configuration) => {
+                context(`${configuration}`, function () {
+                  configuration.forEach((component) => {
+                    context(`${component}`, function () {
+                      it('should only use allowable requirements', function () {
+                        //return true;
+                        component.should.be.oneOf(requirements);
+                      });
+                    });
+                  });
+                });
+            });
+          });
+        });
+      });
     });
   });
 });
